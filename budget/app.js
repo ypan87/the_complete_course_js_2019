9ba45b024eeb98fbbba50a9166da7c01a7bb6de4
panyifan <panyifan@chinac.com>
2019-10-10 10:59:49 +0800

    feature:完成添加后清除输入框内容

--- budget/app.js
+++ budget/app.js
@@ -146,6 +146,20 @@ var UIController = (function() {
 
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus;
         }
     }
 
@@ -172,9 +186,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Add the item to the UI
             UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+            UICtrl.clearFields();
         }
     }
-
 
     return {
         init: function() {
