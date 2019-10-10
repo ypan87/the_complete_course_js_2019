778728e4e7eac0efb14b9a5661eefb8ace9b8657
panyifan <panyifan@chinac.com>
2019-10-10 14:10:00 +0800

    feature:初始化时添加budget

--- budget/app.js
+++ budget/app.js
@@ -398,6 +398,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         init: function() {
             console.log("Application has started.");
             UICtrl.displayMonth();
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     }
